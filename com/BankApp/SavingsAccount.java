71c2f703f35f40ea8ba45c5c110bef4380867f00
yugal47 <yugaltiwari47@gmail.com>
2026-01-09 12:40:01 +0530

    Features to save in savings accounts

---
+++ com/BankApp/SavingsAccount.java
@@ -0,0 +1,17 @@
+package com.BankApp;
+
+public class SavingsAccount {
+    
+}
+class PersonalSavings extends SavingsAccount {
+    
+}
+class VacationSavings extends SavingsAccount {
+    
+}
+class EmergencySavings extends SavingsAccount {
+    
+}
+class NewCarSavings extends SavingsAccount {
+    
+}
