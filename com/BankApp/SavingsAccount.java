3d0e1e55861fb93c579502b3365471dfce9de004
yugal47 <yugaltiwari47@gmail.com>
2026-01-10 21:41:43 +0530

    added vacation savings

--- com/BankApp/SavingsAccount.java
+++ com/BankApp/SavingsAccount.java
@@ -64,9 +64,32 @@ class PersonalSavings extends SavingsAccount {
 }
 }
 class VacationSavings extends SavingsAccount {
+    public void vacationsavings() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        userid =getUserid();
+        int amount;
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+        PreparedStatement ps1 = con.prepareStatement("select * from user where user_id =?");
+        ps1.setInt(1, userid);
+        ResultSet rs1=ps1.executeQuery(); 
+        if(rs1.next()){
+            System.out.println(" Enter the amount you want to deposit in Vacation Savings Account: ");
+            amount = sc.nextInt();
+            PreparedStatement ps2 = con.prepareStatement("INSERT INTO savingsAccount (user_id,VacationSavings) VALUES (?,?)  ON DUPLICATE KEY UPDATE VacationSavings = VacationSavings + values(VacationSavings);");
+            ps2.setInt(1, userid);
+            ps2.setInt(2, amount);
+            int i = ps2.executeUpdate();
+            if(i>0){
+                System.out.println("Successfully deposited " + amount + " in Vacation Savings Account.");
+            }
+            else{
+                System.out.println("Deposit failed. Please try again.");
+        }
+    }
     
 }
 class EmergencySavings extends SavingsAccount {
+    
     
 }
 class NewCarSavings extends SavingsAccount {
