471387f0f2cb0d99d37db5fcd55c208d192c9b9d
yugal47 <yugaltiwari47@gmail.com>
2026-01-10 21:45:12 +0530

    added New car savings feature

--- com/BankApp/SavingsAccount.java
+++ com/BankApp/SavingsAccount.java
@@ -113,5 +113,27 @@ class EmergencySavings extends SavingsAccount {
     }
 }
 class NewCarSavings extends SavingsAccount {
-    
+    public void newcarsavings() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        userid =getUserid();
+        int amount;
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+        PreparedStatement ps1 = con.prepareStatement("select * from user where user_id =?");
+        ps1.setInt(1, userid);
+        ResultSet rs1=ps1.executeQuery(); 
+        if(rs1.next()){
+            System.out.println(" Enter the amount you want to deposit in New Car Savings Account: ");
+            amount = sc.nextInt();
+            PreparedStatement ps2 = con.prepareStatement("INSERT INTO savingsAccount (user_id,NewCarSavings) VALUES (?,?)  ON DUPLICATE KEY UPDATE NewCarSavings = NewCarSavings + values(NewCarSavings);");
+            ps2.setInt(1, userid);
+            ps2.setInt(2, amount);
+            int i = ps2.executeUpdate();
+            if(i>0){
+                System.out.println("Successfully deposited " + amount + " in New Car Savings Account.");
+            }
+            else{
+                System.out.println("Deposit failed. Please try again.");
+            }
+        }
+    }   
 }
