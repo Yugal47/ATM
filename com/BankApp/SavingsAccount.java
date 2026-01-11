0b1304e62d191e20a3f5ca3dd7ecd8aa99328bed
yugal47 <yugaltiwari47@gmail.com>
2026-01-11 23:45:03 +0530

    modifications in code

--- com/BankApp/SavingsAccount.java
+++ com/BankApp/SavingsAccount.java
@@ -40,8 +40,7 @@ public class SavingsAccount {
         if (rs1.next()) {
             userid = rs1.getInt("user_id");
             // System.out.println(userid);
-            // displaySavingsAccountOptions();
-
+            
         }
     }
 
@@ -57,12 +56,12 @@ public class SavingsAccount {
         // int ch = 1;
         switch (ch) {
             case 1:
-                SavingsAccount ps = new PersonalSavings();
-                ps.accountDetails();
-                accountDetails();
+                PersonalSavings ps = new PersonalSavings(); 
+                ps.personalSavings();
                 break;
             case 2:
-                accountDetails();
+                VacationSavings vs = new VacationSavings();
+                vs.vacationSavings();
                 break;
             case 3:
                 accountDetails();
@@ -73,37 +72,12 @@ public class SavingsAccount {
             default:
                 System.out.println(" Invalid choice. Please try again.");
                 break;
-            }
+        }
     }
     
 }
 
-class PersonalSavings extends SavingsAccount {
-    // @Override
-    public void personalSavings() throws Exception {
-        Scanner sc = new Scanner(System.in);
-        userid = getUserid();
-        int amount;
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
-        PreparedStatement ps1 = con.prepareStatement("select * from user where user_id =?");
-        ps1.setInt(1, userid);
-        ResultSet rs1 = ps1.executeQuery();
-        if (rs1.next()) {
-            System.out.println(" Enter the amount you want to deposit in Personal Savings Account: ");
-            amount = sc.nextInt();
-            PreparedStatement ps2 = con.prepareStatement(
-                    "INSERT INTO savingsAccount (user_id,PersonalSavings) VALUES (?,?)  ON DUPLICATE KEY UPDATE PersonalSavings = PersonalSavings + values(PersonalSavings);");
-            ps2.setInt(1, userid);
-            ps2.setInt(2, amount);
-            int i = ps2.executeUpdate();
-            if (i > 0) {
-                System.out.println("Successfully deposited " + amount + " in Personal Savings Account.");
-            } else {
-                System.out.println("Deposit failed. Please try again.");
-            }
-        }
-    }
-}
+
 
 class VacationSavings extends SavingsAccount {
     // @Override
