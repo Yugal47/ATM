f4f25aee65e7f0a5799e35c267c5fdafe0c92788
yugal47 <yugaltiwari47@gmail.com>
2026-01-12 22:49:32 +0530

    code not woring

--- com/BankApp/SavingsAccount.java
+++ com/BankApp/SavingsAccount.java
@@ -56,6 +56,7 @@ public class SavingsAccount {
         // int ch = 1;
         switch (ch) {
             case 1:
+                accountDetails();
                 PersonalSavings ps = new PersonalSavings(); 
                 ps.personalSavings();
                 break;
@@ -64,10 +65,12 @@ public class SavingsAccount {
                 vs.vacationSavings();
                 break;
             case 3:
-                accountDetails();
+                EmergencySavings es = new EmergencySavings();
+                es.emergencySavings();
                 break;
             case 4:
-                accountDetails();
+                NewCarSavings ncs = new NewCarSavings();
+                ncs.newCarSavings();
                 break;
             default:
                 System.out.println(" Invalid choice. Please try again.");
@@ -77,13 +80,39 @@ public class SavingsAccount {
     
 }
 
+class PersonalSavings{
+    SavingsAccount sa = new SavingsAccount();
+    int userid=sa.getUserid();
+    public void personalSavings() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int amount;
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+        PreparedStatement ps1 = con.prepareStatement("select * from user where user_id =?");
+        ps1.setInt(1, userid);
+        ResultSet rs1 = ps1.executeQuery();
+        if (rs1.next()) {
+            System.out.println(" Enter the amount you want to deposit in Personal Savings Account: ");
+            amount = sc.nextInt();
+            PreparedStatement ps2 = con.prepareStatement(
+                    "INSERT INTO savingsAccount (user_id,PersonalSavings) VALUES (?,?)  ON DUPLICATE KEY UPDATE PersonalSavings = PersonalSavings + values(PersonalSavings);");
+            ps2.setInt(1, userid);
+            ps2.setInt(2, amount);
+            int i = ps2.executeUpdate();
+            if (i > 0) {
+                System.out.println("Successfully deposited " + amount + " in Personal Savings Account.");
+            } else {
+                System.out.println("Deposit failed. Please try again.");
+            }
+        }
+    }
+}
 
-
-class VacationSavings extends SavingsAccount {
+class VacationSavings {
+    SavingsAccount sa = new SavingsAccount();
+    int userid=sa.getUserid();
     // @Override
     public void vacationSavings() throws Exception {
         Scanner sc = new Scanner(System.in);
-        userid = getUserid();
         int amount;
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
         PreparedStatement ps1 = con.prepareStatement("select * from user where user_id =?");
@@ -102,15 +131,19 @@ class VacationSavings extends SavingsAccount {
             } else {
                 System.out.println("Deposit failed. Please try again.");
             }
+            ps1.close();
+            ps2.close();
+            con.close();
         }
     }
 
 }
 
-class EmergencySavings extends SavingsAccount {
-    public void accountDetails() throws Exception {
+class EmergencySavings {
+    SavingsAccount sa = new SavingsAccount();
+    int userid=sa.getUserid();
+    public void emergencySavings() throws Exception {
         Scanner sc = new Scanner(System.in);
-        userid = getUserid();
         int amount;
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
         PreparedStatement ps1 = con.prepareStatement("select * from user where user_id =?");
@@ -134,7 +167,7 @@ class EmergencySavings extends SavingsAccount {
 }
 
 class NewCarSavings extends SavingsAccount {
-    public void accountDetails() throws Exception {
+    public void newCarSavings() throws Exception {
         Scanner sc = new Scanner(System.in);
         userid = getUserid();
         int amount;
