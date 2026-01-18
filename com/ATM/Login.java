0e408df0f8f8cca2d90e4509e73d5b9947c2ea9d
yugal47 <yugaltiwari47@gmail.com>
2026-01-18 11:01:57 +0530

    Removed static method

--- com/ATM/Login.java
+++ com/ATM/Login.java
@@ -112,7 +112,7 @@ public class Login {
         this.userid = userid;
     }
     
-    public static String hashPassword(String pin) {
+    public String hashPassword(String pin) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] hashBytes = md.digest(pin.getBytes("UTF-8"));
